32a65b674b7f0334e915a3495d4b10e980c6928e
Aldofert0 <aldorodrigo08@gmail.com>
2025-10-08 12:06:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function LikeNinja(element) {
+    let pulsador = ParseInd(element.innertext)
+    count ++;
+    element.innertext = count + "vistas"
+}
+
+function addlike(element) {
+    let pulsador = parseInt(element.innertext)
+    count ++ ;
+    element.innertext = count + "vistas"
+}
